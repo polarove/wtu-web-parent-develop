61a258523d937c433d9129295ffcb92dfca6ba0f
Liuqi <liuqi6602@163.com>
2023-09-28 19:37:05 +0800

    组队TeamBO添加队伍更新时间

--- wtu-web-backend/src/main/java/cn/neorae/wtu/module/team/domain/bo/TeamBO.java
+++ wtu-web-backend/src/main/java/cn/neorae/wtu/module/team/domain/bo/TeamBO.java
@@ -3,12 +3,18 @@ package cn.neorae.wtu.module.team.domain.bo;
 
 import lombok.Data;
 
+import java.util.Date;
+
 @Data
 public class TeamBO {
 
     private Integer id;
 
+
     private String uuid;
+
+
+    private Date updateTime;
 
 
     private Integer server;
